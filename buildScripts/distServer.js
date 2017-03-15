1776e8c9b66731ed6606783051824a22706b5ac9
Serhiy Konyk <konyk93@gmail.com>
2017-03-14 23:55:13 -0400

    Created production build process

---
+++ buildScripts/distServer.js
@@ -0,0 +1,52 @@
+import express from 'express';
+import path from 'path';
+import open from 'open';
+import compression from 'compression';
+
+/* eslint-disable no-console */
+
+const port = 3000;
+const app = express();
+
+app.use(compression());
+app.use(express.static('dist'));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, '../dist/index.html'));
+});
+
+app.get('/users', function (req, res) {
+  res.json([{
+      "id": 1,
+      "firstName": "Bob",
+      "lastName": "Smith",
+      "email": "test@mail.com"
+    },
+    {
+      "id": 1,
+      "firstName": "Bob",
+      "lastName": "Smith",
+      "email": "test@mail.com"
+    },
+    {
+      "id": 1,
+      "firstName": "Bob",
+      "lastName": "Smith",
+      "email": "test@mail.com"
+    },
+    {
+      "id": 1,
+      "firstName": "Bob",
+      "lastName": "Smith",
+      "email": "test@mail.com"
+    }
+  ]);
+});
+
+app.listen(port, function (err) {
+  if (err) {
+    console.loc(err);
+  } else {
+    open('http://localhost:' + port);
+  }
+});
--- buildScripts/generateMockData.js
+++ buildScripts/generateMockData.js
@@ -5,6 +5,8 @@ import {
 import fs from 'fs';
 import chalk from 'chalk';
 
+/* eslint-disable no-console */
+
 const json = JSON.stringify(jsf(schema));
 
 fs.writeFile('./src/api/db.json', json, function (err) {
