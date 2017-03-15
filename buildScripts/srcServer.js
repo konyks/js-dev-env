681036440ab73cfc608cf8941d038d22cdfb79df
Serhiy Konyk <konyk93@gmail.com>
2017-03-14 22:20:33 -0400

    Added simple api with mock data.

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -19,6 +19,34 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', function (req, res) {
+  res.json([{
+      "id": 1,
+      "firstName": "Bob",
+      "lastName": "Smith",
+      "email": "test@mail.com"
+    },
+    {
+      "id": 1,
+      "firstName": "Bob",
+      "lastName": "Smith",
+      "email": "test@mail.com"
+    },
+    {
+      "id": 1,
+      "firstName": "Bob",
+      "lastName": "Smith",
+      "email": "test@mail.com"
+    },
+    {
+      "id": 1,
+      "firstName": "Bob",
+      "lastName": "Smith",
+      "email": "test@mail.com"
+    }
+  ]);
+});
+
 app.listen(port, function (err) {
   if (err) {
     console.loc(err);
